f89fdd155dc1994f133de6be50cbb78508da2150
zeekrey <christian@krey.io>
2025-12-12 15:51:31 +0100

    adjust db path

--- examples/natural-language-gis/drizzle.config.ts
+++ examples/natural-language-gis/drizzle.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
+console.log("Using:", process.env.DATABASE_URL);
+
 export default defineConfig({
   schema: "./src/lib/server/db/schema.ts",
   dialect: "postgresql",
